2a6b156994db24007d83293d592c49ee6a9fc62b
Chris Yammine <chris.yammine@hotmail.com>
2014-08-16 18:34:50 -0700

    Add comments to controller to make it easier to understand what's going on

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -7,28 +7,28 @@ class PricesController < ApplicationController
   def update_prices
     
     if Price.last
-      previous_cad = Price.last.CAD
-      previous_cny = Price.last.CNY
+      previous_cad = Price.last.CAD # Assign previous_cad to the last recorded CAD price in DB
+      previous_cny = Price.last.CNY # Assign previous_cny to the last recorded CNY price in DB
     end
     
-    if Price.recent.last
+    if Price.recent.last # If a price has been grabbed in the last 14 seconds then just pull from DB
       @price = Price.last
     else 
-      begin 
-        Timeout::timeout(DEFAULT_TIMEOUT) {
+      begin # Check bitcoinaverage for latest price
+        Timeout::timeout(10) { # Throw Timeout::Error after 10 seconds
           cad = HTTParty.get("https://api.bitcoinaverage.com/ticker/global/CAD/")
           cny = HTTParty.get("https://api.bitcoinaverage.com/ticker/global/CNY/")
-          previous_cad ||= cad["last"]
-          previous_cny ||= cny["last"]
+          previous_cad ||= cad["last"] # Assign previous_cad to current CAD price if no previous price exists
+          previous_cny ||= cny["last"] # Assign previous_cny to current CNY price if no previous price exists
           @price = Price.new(:CAD => cad["last"], :CNY => cny["last"],
                              time: cad["timestamp"], prevCAD: previous_cad,
                              prevCNY: previous_cny)
           @price.save
         }
       rescue => e
-        if Price.last
+        if Price.last # Output last price recorded if Timeout occurs
           @price = Price.last
-        else
+        else # If no previous price exists - show unavailable
           @price = { :CAD => "Unavailable", :CNY => "Unavailable",
                      time: Time.now, prevCAD: "Unavailable",
                      prevCNY: "Unavailable" }
--- app/models/price.rb
+++ app/models/price.rb
@@ -1,10 +1,5 @@
 class Price < ActiveRecord::Base
 
-
-  DEFAULT_TIMEOUT = 10
-
   scope :recent, -> { where([ "time > ?", 14.seconds.ago ])}
-
-
 
 end
